6fa081e878c6382fe622623cb6258aeb9899ce28
Stephen Ribich <stephen.ribich@gmail.com>
2017-09-19 15:37:35 -0400

    update license to GNU GPLv3

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
-## Copyright (c) 2017 Stephen Ribich
+## Copyright (c) 2017 Stephen Ribich <stephen.ribich@gmail.com>
 ##
-## Permission is hereby granted, free of charge, to any person obtaining a copy
-## of this software and associated documentation files (the "Software"), to deal
-## in the Software without restriction, including without limitation the rights
-## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-## copies of the Software, and to permit persons to whom the Software is
-## furnished to do so, subject to the following conditions:
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
 ##
-## The above copyright notice and this permission notice shall be included in
-## all copies or substantial portions of the Software.
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
 ##
-## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-## SOFTWARE.
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # shellcheck disable=1091
 
@@ -25,12 +20,13 @@ cd "$(dirname "$(readlink -f "$0" || realpath "$0")")" || :
 
 source "./lib/print"
 source "./lib/menu"
-source "./lib/boot_mode"
 source "./lib/device"
-source "./lib/network"
 source "./lib/control_flow"
 source "./lib/misc"
 source "./lib/selection"
+
+source "./lib/configure/boot_mode"
+source "./lib/configure/connection"
 
 source "./lib/step01_keymap"
 source "./lib/step02_editor"
@@ -38,51 +34,33 @@ source "./lib/step03_partition_disk"
 
 SCRIPT_TITLE="Arch Bootstrap"
 
-arch_chroot()
-{
-  exit 0
-}
+################################################################################
+## Script Configuration
+################################################################################
 
-## Set up the script for proper execution
-##
 configure()
 {
-  print_title "${SCRIPT_TITLE}"
+    get_boot_mode
 
-  get_boot_mode
+    if is_bios; then
+        printf "BIOS mode is not currently supported for this script\n"
+        exit 1
+    fi
 
-  if [[ "${UEFI}" -eq 0 ]]; then
-    printf "BIOS mode is not currently supported for this script"
-    exit 1
-  fi
-
-  check_connection
+    check_connection
 }
 
-## Update the package repository
-##
 sync()
 {
-  pacman -Sy
+    pacman -Sy
 }
 
-## Ran when the script finishes
-##
-finish()
+################################################################################
+## Menu Selection
+################################################################################
+
+print_menu()
 {
-  exit 0
-}
-
-main()
-{
-  configure
-  sync
-
-  while :; do
-    local option
-
-    print_title "${SCRIPT_TITLE}"
-
     printf " 1) %s\n" "$(mainmenu_item "${checklist[1]}"  "Select Keymap" "${KEYMAP}")"
     printf " 2) %s\n" "$(mainmenu_item "${checklist[2]}"  "Select Editor" "${EDITOR}")"
     printf " 3) %s\n" "$(mainmenu_item "${checklist[3]}"  "Partition Disk" "")"
@@ -99,40 +77,58 @@ main()
     printf "\n"
     printf " d) %s\n" "Done"
     printf "\n"
+}
 
-    read -r -p "${SELECTION_PROMPT}" option
+################################################################################
+##
+################################################################################
+main()
+{
+    print_title "${SCRIPT_TITLE}"
 
-    case "${option}" in
-      1)
-        select_keymap
-        checklist[1]=1
-        ;;
-      2)
-        select_editor
-        checklist[2]=1
-        ;;
-      3)
-        partition_disk
-        checklist[3]=1
-        ;;
-      4)
-        install_base_system
-        configure_keymap
-        configure_dns
-        checklist[4]=1
-        ;;
-      5)
-        configure_fstab
-        checklist[5]=1
-        ;;
-      d)
-        finish
-        ;;
-      *)
-        invalid_option
-        ;;
-    esac
-  done
+    configure
+    sync
+
+    while :; do
+        local option
+
+        print_title "${SCRIPT_TITLE}"
+
+        print_menu
+
+        read -r -p "${SELECTION_PROMPT}" option
+
+        case "${option}" in
+            1)
+                select_keymap
+                checklist[1]=1
+                ;;
+            2)
+                select_editor
+                checklist[2]=1
+                ;;
+            3)
+                partition_disk
+                checklist[3]=1
+                ;;
+            4)
+                install_base_system
+                configure_keymap
+                configure_dns
+                checklist[4]=1
+                ;;
+            5)
+                configure_fstab
+                checklist[5]=1
+                ;;
+            d)
+                finish
+                ;;
+            *)
+                invalid_option
+                ;;
+        esac
+    done
 }
 
 main "$@"
