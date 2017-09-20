67655eea5b9a7bb5f5af15d1426f8229b11b613b
Stephen Ribich <stephen.ribich@gmail.com>
2017-09-19 20:08:14 -0400

    pull menu logic out of main

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,19 +15,17 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# shellcheck disable=1091
-
 cd "$(dirname "$(readlink -f "$0" || realpath "$0")")" || :
 
-source "./lib/print"
-source "./lib/menu"
-source "./lib/device"
-source "./lib/control_flow"
-source "./lib/misc"
-source "./lib/selection"
+source "./lib/util/print"
+source "./lib/util/menu"
+source "./lib/util/device"
+source "./lib/util/control_flow"
+source "./lib/util/misc"
+source "./lib/util/selection"
 
-source "./lib/configure/boot_mode"
-source "./lib/configure/connection"
+source "./lib/boot_mode"
+source "./lib/connection"
 
 source "./lib/step01_keymap"
 source "./lib/step02_editor"
@@ -62,22 +60,96 @@ sync()
 
 print_menu()
 {
-    printf " 1) %s\n" "$(mainmenu_item "${checklist[1]}"  "Select Keymap" "${KEYMAP}")"
-    printf " 2) %s\n" "$(mainmenu_item "${checklist[2]}"  "Select Editor" "${EDITOR}")"
-    printf " 3) %s\n" "$(mainmenu_item "${checklist[3]}"  "Partition Disk" "")"
-    printf " 4) %s\n" "$(mainmenu_item "${checklist[4]}"  "Install Base System" "")"
-    printf " 5) %s\n" "$(mainmenu_item "${checklist[5]}"  "Configure Fstab" "")"
-    printf " 6) %s\n" "$(mainmenu_item "${checklist[6]}"  "Configure Hostname" "")"
-    printf " 7) %s\n" "$(mainmenu_item "${checklist[7]}"  "Configure Timezone" "")"
-    printf " 8) %s\n" "$(mainmenu_item "${checklist[8]}"  "Configure Hardware Clock" "")"
-    printf " 9) %s\n" "$(mainmenu_item "${checklist[9]}"  "Configure Locale" "")"
-    printf "10) %s\n" "$(mainmenu_item "${checklist[10]}" "Configure Mkinitcpio" "")"
-    printf "11) %s\n" "$(mainmenu_item "${checklist[11]}" "Install Bootloader" "")"
-    printf "12) %s\n" "$(mainmenu_item "${checklist[12]}" "Root Password" "")"
+    printf " 1) %s\n" "$(mainmenu_item                  \
+                             "${checklist[1]}"          \
+                             "Select Keymap"            \
+                             "${KEYMAP}")"
+    printf " 2) %s\n" "$(mainmenu_item                  \
+                             "${checklist[2]}"          \
+                             "Select Editor"            \
+                             "${EDITOR}")"
+    printf " 3) %s\n" "$(mainmenu_item                  \
+                             "${checklist[3]}"          \
+                             "Partition Disk"           \
+                             "")"
+    printf " 4) %s\n" "$(mainmenu_item                  \
+                             "${checklist[4]}"          \
+                             "Install Base System"      \
+                             "")"
+    printf " 5) %s\n" "$(mainmenu_item                  \
+                             "${checklist[5]}"          \
+                             "Configure Fstab"          \
+                             "")"
+    printf " 6) %s\n" "$(mainmenu_item                  \
+                             "${checklist[6]}"          \
+                             "Configure Hostname"       \
+                             "")"
+    printf " 7) %s\n" "$(mainmenu_item                  \
+                             "${checklist[7]}"          \
+                             "Configure Timezone"       \
+                             "")"
+    printf " 8) %s\n" "$(mainmenu_item                  \
+                             "${checklist[8]}"          \
+                             "Configure Hardware Clock" \
+                             "")"
+    printf " 9) %s\n" "$(mainmenu_item                  \
+                             "${checklist[9]}"          \
+                             "Configure Locale"         \
+                             "")"
+    printf "10) %s\n" "$(mainmenu_item                  \
+                             "${checklist[10]}"         \
+                             "Configure Mkinitcpio"     \
+                             "")"
+    printf "11) %s\n" "$(mainmenu_item                  \
+                             "${checklist[11]}"         \
+                             "Install Bootloader"       \
+                             "")"
+    printf "12) %s\n" "$(mainmenu_item                  \
+                             "${checklist[12]}"         \
+                             "Root Password"            \
+                             "")"
 
     printf "\n"
     printf " d) %s\n" "Done"
     printf "\n"
+}
+
+print_options()
+{
+    local option
+
+    read -r -p "${SELECTION_PROMPT}" option
+
+    case "${option}" in
+        1)
+            select_keymap
+            checklist[1]=1
+            ;;
+        2)
+            select_editor
+            checklist[2]=1
+            ;;
+        3)
+            partition_disk
+            checklist[3]=1
+            ;;
+        4)
+            install_base_system
+            configure_keymap
+            configure_dns
+            checklist[4]=1
+            ;;
+        5)
+            configure_fstab
+            checklist[5]=1
+            ;;
+        d)
+            finish
+            ;;
+        *)
+            invalid_option
+            ;;
+    esac
 }
 
 ################################################################################
@@ -91,44 +163,10 @@ main()
     sync
 
     while :; do
-        local option
-
         print_title "${SCRIPT_TITLE}"
 
         print_menu
-
-        read -r -p "${SELECTION_PROMPT}" option
-
-        case "${option}" in
-            1)
-                select_keymap
-                checklist[1]=1
-                ;;
-            2)
-                select_editor
-                checklist[2]=1
-                ;;
-            3)
-                partition_disk
-                checklist[3]=1
-                ;;
-            4)
-                install_base_system
-                configure_keymap
-                configure_dns
-                checklist[4]=1
-                ;;
-            5)
-                configure_fstab
-                checklist[5]=1
-                ;;
-            d)
-                finish
-                ;;
-            *)
-                invalid_option
-                ;;
-        esac
+        print_options
     done
 }
 
